f41febe1b54b7942c3b299092e996889cff7c833
Tony Pilz <tonypilzj@gmx.de>
2017-06-17 11:10:41 +0200

    added single file

--- devel/tools/SingleFileGenerator.cpp
+++ devel/tools/SingleFileGenerator.cpp
@@ -1,21 +1,24 @@
 #include "SingleFileGenerator.h"
 
 //1. enable defines
-//2. compile with #QMAKE_CXXFLAGS += -E
-//3. open SingleFileGenerator.o with notepad
-//4. search for "Units\\" to find the starting point
-//5. copy "Units\\" code to SingleFile.h
-//6. remove "#.+"
-//7. replace "\r\n +\r\n" -> "\r\n\r\n"
-//7. remove "\}(\r\n)+namespace unit \{\r\n" in notepad
+//2. recompile with #QMAKE_CXXFLAGS += -E
+//3. open SingleFileGenerator.o with texteditor
+//4. remove "#.+" (regex)
+//5. replace "\r\n +\r\n" -> "\r\n\r\n" (regex)
+//6. remove "\}(\r\n)+namespace unit \{\r\n" (regex)
+//7. copy the unit-namespace to SingleFile.h
+//8. add includes
+//9. beautify everything except milli-block and everything starting at namespace u
 
-
+//#define _GLIBCXX_IOSFWD 1
+//#define _GLIBCXX_OSTREAM 1
 //#define _GLIBCXX_RATIO 1
 //#define _GLIBCXX_CTGMATH 1
 //#define _GLIBCXX_TYPE_TRAITS 1
 //#define _GLIBCXX_IOSTREAM 1
 //#define _GLIBCXX_SSTREAM 1
 //#define _GLIBCXX_STRING	1
+//#define _GLIBCXX_ISTREAM 1
 
 
 
--- devel/tools/SingleFileTester.cpp
+++ devel/tools/SingleFileTester.cpp
@@ -2,22 +2,12 @@
 
 #include "SingleFile.h"
 
-#include <src/quantity/quantityDefinitions.h>
-#include <src/quantity/quantityOperators.h>
-
-#include <ratio>
-#include <ctgmath>
-#include <type_traits>
-#include <iostream>
-#include <sstream>
-#include <string>
-
 
 
 int rrun(int t) {
 
-    using namespace unit;
-    using namespace unit::literals;
+    using namespace singleFile::unit;
+    using namespace singleFile::unit::literals;
 
     //cast ctor
     newton cz1 = 3.4_newton + Quantity<u::newton, int> {kilo(t)};
@@ -28,13 +18,15 @@ int rrun(int t) {
         cz2 += Quantity<u::metre_per_second_squared>{micro(static_cast<double>(i))};
     }
 
-    return unit::static_unit_cast<int>(cz2).magnitude();
+    return static_unit_cast<int>(cz2).magnitude();
 }
 
 SingleFileTester::SingleFileTester()
 {
 
     rrun(4);
+
+    std::cout<<__FILE__<<"\n";
 
 
 }
