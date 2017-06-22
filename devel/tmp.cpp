c1d1b2d9732fae588ae1518d02f5673809f693d2
Tony Pilz <tonypilzj@gmx.de>
2017-06-22 17:30:48 +0200

    cosmetic changes

--- devel/tmp.cpp
+++ devel/tmp.cpp
@@ -2,6 +2,7 @@
 #include <tests/unit_test/odr_test/UnitHeaders.h>
 #include <src/quantity/quantityDefinitionsSI.h>
 #include <tools/pysConstsantsGeneration/generated/physicalConstants.h>
+#include <src/quantity/quantityPrinting.h>
 
 #include <iostream>
 #include <sstream>
@@ -19,13 +20,17 @@ int run()
 
     std::cout<<(physicalConstant::atomic_unit_of_2nd_hyperpolarizability* physicalConstant::uncertaintiy::atomic_unit_of_2nd_hyperpolarizability)<<"\n";
 
-    t::newton force = 2.0_n* meter * 3.0_n * kilogram / math::square(2.0_n * second);
+    t::newton some_force = 2.0_n*meter * 3.0_n*kilogram / math::square(2.0_n*second);
 
-    std::cout<<force<<"\n"; //prints "1.5N"
+    std::cout<<some_force<<"\n"; //prints "1.5N"
 
-    force+=5.0_newton;
+    some_force = some_force + 2.0_n * newton; // ok
 
-    std::cout<<force<<"\n"; //prints "6.5N"
+    some_force+=5.0_newton;
+
+    std::cout<<some_force<<"\n"; //prints "6.5N"
+
+    std::cout<<math::cube( 500.0_n * deci(inch) ) <<"\n"; //f == 3.53961e-009m^3
 
     return 0;
 
